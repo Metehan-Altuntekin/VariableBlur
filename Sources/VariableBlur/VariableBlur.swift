081b120589a1670842a8c3d1a1cad824c723be84
Mete <86600423+Metehan-Altuntekin@users.noreply.github.com>
2026-03-09 16:34:12 +0300

    Add endOffset configuration option

--- Sources/VariableBlur/VariableBlur.swift
+++ Sources/VariableBlur/VariableBlur.swift
@@ -19,11 +19,13 @@ public struct VariableBlurView: UIViewRepresentable {
     
     /// By default, variable blur starts from 0 blur radius and linearly increases to `maxBlurRadius`. Setting `startOffset` to a small negative coefficient (e.g. -0.1) will start blur from larger radius value which might look better in some cases.
     public var startOffset: CGFloat = 0
+    public var endOffset: CGFloat = 1
     
-    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
+    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0, endOffset: CGFloat = 1) {
         self.maxBlurRadius = maxBlurRadius
         self.direction = direction
         self.startOffset = startOffset
+        self.endOffset = endOffset  // 1.0 = full height of view, 0.5 = half, etc.
     }
     
     public func makeUIView(context: Context) -> VariableBlurUIView {
@@ -34,11 +36,10 @@ public struct VariableBlurView: UIViewRepresentable {
     }
 }
 
-
 /// credit https://github.com/jtrivedi/VariableBlurView
 open class VariableBlurUIView: UIVisualEffectView {
 
-    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0) {
+    public init(maxBlurRadius: CGFloat = 20, direction: VariableBlurDirection = .blurredTopClearBottom, startOffset: CGFloat = 0, endOffset: CGFloat = 1) {
         super.init(effect: UIBlurEffect(style: .regular))
 
         let clsName = String("retliFAC".reversed())
@@ -54,7 +55,7 @@ open class VariableBlurUIView: UIVisualEffectView {
 
         // The blur radius at each pixel depends on the alpha value of the corresponding pixel in the gradient mask.
         // An alpha of 1 results in the max blur radius, while an alpha of 0 is completely unblurred.
-        let gradientImage = makeGradientImage(startOffset: startOffset, direction: direction)
+        let gradientImage = makeGradientImage(startOffset: startOffset, endOffset: endOffset, direction: direction)
 
         variableBlur.setValue(maxBlurRadius, forKey: "inputRadius")
         variableBlur.setValue(gradientImage, forKey: "inputMaskImage")
@@ -63,7 +64,7 @@ open class VariableBlurUIView: UIVisualEffectView {
         // We use a `UIVisualEffectView` here purely to get access to its `CABackdropLayer`,
         // which is able to apply various, real-time CAFilters onto the views underneath.
         let backdropLayer = subviews.first?.layer
-
+        
         // Replace the standard filters (i.e. `gaussianBlur`, `colorSaturate`, etc.) with only the variableBlur.
         backdropLayer?.filters = [variableBlur]
         
@@ -72,7 +73,7 @@ open class VariableBlurUIView: UIVisualEffectView {
             subview.alpha = 0
         }
     }
-
+    
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -87,13 +88,13 @@ open class VariableBlurUIView: UIVisualEffectView {
         // `super.traitCollectionDidChange(previousTraitCollection)` crashes the app
     }
     
-    private func makeGradientImage(width: CGFloat = 100, height: CGFloat = 100, startOffset: CGFloat, direction: VariableBlurDirection) -> CGImage { // much lower resolution might be acceptable
+    private func makeGradientImage(width: CGFloat = 100, height: CGFloat = 100, startOffset: CGFloat, endOffset: CGFloat, direction: VariableBlurDirection) -> CGImage { // much lower resolution might be acceptable
         let ciGradientFilter =  CIFilter.linearGradient()
 //        let ciGradientFilter =  CIFilter.smoothLinearGradient()
         ciGradientFilter.color0 = CIColor.black
         ciGradientFilter.color1 = CIColor.clear
         ciGradientFilter.point0 = CGPoint(x: 0, y: height)
-        ciGradientFilter.point1 = CGPoint(x: 0, y: startOffset * height) // small negative value looks better with vertical lines
+        ciGradientFilter.point1 = CGPoint(x: 0, y: height * (1 - endOffset)) // fades to zero over endOffset * height
         if case .blurredBottomClearTop = direction {
             ciGradientFilter.point0.y = 0
             ciGradientFilter.point1.y = height - ciGradientFilter.point1.y
